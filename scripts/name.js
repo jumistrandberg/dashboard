3499167cd0e3f991af9201794b542c9e0783545b
Jumi <feliciastrandberg@gmail.com>
2023-12-28 20:11:34 +0100

    name input text saved input to local storage. Need to fix script to retrieve the save on reload

--- scripts/name.js
+++ scripts/name.js
@@ -1,3 +1,20 @@
 // Get the name section 
 const nameSection = document.querySelector('.name-section'); 
 
+// Get the input textbox
+const nameTextBox = document.getElementById('nameTextBox');
+
+// Listen for clicks on the text input 
+nameTextBox.addEventListener('click', function(input) {
+    nameTextBox.value = ''
+});
+
+// Listen for changes to input 
+nameTextBox.addEventListener('input', function() {
+    // Remove end white space 
+    const enteredText = nameTextBox.value.trim();
+    if (enteredText !== '') {
+        localStorage.setItem('savedText', enteredText);
+    }
+})
+
