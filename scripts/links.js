b46308f5bf4707f3d1597be03601e0fe06912660
Jumi <feliciastrandberg@gmail.com>
2023-12-29 13:40:37 +0100

    stored the dialog elements needed in consts from the DOM

---
+++ scripts/links.js
@@ -0,0 +1,7 @@
+// Dialog functionality 
+// Get the dialog
+const dialog = document.querySelector('dialog');
+// Get the open dialog button (add link)
+const openDialogBtn = document.querySelector('dialog + button'); 
+// Get the close dialog button 
+const closeDialogBtn = document.querySelector('dialog button'); 
