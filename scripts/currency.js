86e829d1415fbf69fb01d6cda713912edbcf1b0d
Jumi <feliciastrandberg@gmail.com>
2023-12-30 19:45:51 +0100

    added inputs for currency to html and put them in variables in the script

--- scripts/currency.js
+++ scripts/currency.js
@@ -1,10 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Get the base URL
-  const apiUrl =
-    'https://api.exchangeratesapi.io/v1/latest?access_key=';
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the elements
+    const currencyFrom = document.getElementById('currency-from'); 
+    const currencyTo = document.getElementById('currency-to');
+    const currencyAmount = document.getElementById('currency-amount'); 
+    
+  // endpoint to convert
+  const endpoint = "convert";
 
+  // Get the base URL
+  const url = `https://api.exchangeratesapi.io/v1/${endpoint}?access_key=${apiKey}&from=${from}&to=${to}&amount=${amount}`; //   endpoint for conversion
+
+  // Fetch the API key
   async function getApiKey() {
-    const response = await fetch('./apiKeys.json');
+    const response = await fetch("./apiKeys.json");
     if (response.ok) {
       const keyArray = await response.json();
       const ApiKey = keyArray[1].currency;
@@ -13,6 +21,33 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(`Error: ${response.status}`);
     }
   }
+
+  //   const access_key = 'API_KEY'; // Replace 'API_KEY' with your actual API key
+
+  //   const from = 'EUR';
+  //   const to = 'GBP';
+  //   const amount = '10';
+
+  //   async function performConversion() {
+  //     try {
+  //       const response = await fetch(url, {
+  //         method: 'GET',
+  //         dataType: 'jsonp',
+  //       });
+
+  //       if (response.ok) {
+  //         const json = await response.json();
+  //         const result = json.result;
+  //         alert(result); // Access the conversion result
+  //       } else {
+  //         throw new Error('Network response was not ok.');
+  //       }
+  //     } catch (error) {
+  //       console.error('Error:', error);
+  //     }
+  //   }
+
+  //   performConversion();
 
   getApiKey();
 });
