4b23b4560402f5769f5171b0b46f3c21436ff143
Jumi <feliciastrandberg@gmail.com>
2023-12-31 00:15:19 +0100

    convert button maybe works. Convert function doesnt work yet so Im not completely sure

--- scripts/currency.js
+++ scripts/currency.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+    
   // Get the elements
   const currencyFrom = document.getElementById("currency-from");
   const currencyTo = document.getElementById("currency-to");
   const currencyAmount = document.getElementById("currency-amount");
   const currencyResult = document.getElementById("currency-result");
+  const currencyBtn = document.getElementById('currency-btn'); 
 
   // endpoint to convert
   const endpoint = "convert";
@@ -48,7 +50,19 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // Listen for input in
+  // Listen for click on button 
+  currencyBtn.addEventListener('click', () => {
+    currencyResult.innerText = '';
+     // Get the user values 
+     const fromCurrency = currencyFrom.value;
+     const toCurrency = currencyTo.value;
+     const amount = currencyAmount.value;
+    if(fromCurrency !== '' && toCurrency !== '' && amount !== '') {
+        convertCurrency();
+    } else {
+        currencyResult.innerText = 'Fill out all fields';
+    }
+  })
   
 
   //   const access_key = 'API_KEY'; // Replace 'API_KEY' with your actual API key
