7935fa21d8f6e2d9e9edd1558902fe317a5c01a9
Jumi <feliciastrandberg@gmail.com>
2023-12-30 15:28:38 +0100

    Starting over. Get apiKey function successfully logs the api key object array from the json file

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,44 +1,18 @@
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+// Get the elements 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
+// Get the base URL 
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+// Function to get the API key 
 async function getApiKey() {
-  try {
-    const response = await fetch("./apiKeys.json");
-    if (response.ok) {
-      const { weather } = await response.json();
-      return weather; 
-    } else {
-      throw new Error(`Failed to fetch API key. Status: ${response.status}`);
+    const response = await fetch('./apiKeys.json')
+    if(response.ok) {
+        const keyArray = await response.json(); 
+        console.log(keyArray);
     }
-  } catch (error) {
-    console.error("Error fetching API key:", error);
-  }
 }
 
-async function checkWeather(city) {
-  try {
-    const apiKey = await getApiKey();
-    if (!apiKey) {
-      console.error("API key is missing or invalid.");
-      return;
-    }
-
-    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
-    const data = await response.json();
-
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
-  } catch (error) {
-    console.error("Error getting weather data:", error);
-  }
-}
-
-searchBtn.addEventListener("click", () => {
-  checkWeather(searchBox.value);
-  searchBox.value = "";
-});
+getApiKey();
