db4a588b33dbf5165270c9954562979a7be80249
Jumi <feliciastrandberg@gmail.com>
2023-12-31 02:46:15 +0100

    creating and displaying a catch button works

--- scripts/poke.js
+++ scripts/poke.js
@@ -7,21 +7,32 @@ const url = 'https://pokeapi.co/api/v2/pokemon-species/';
 const randomPokeNum = Math.floor(Math.random() * 1025 + 1);
 // The name of the random pokemon 
 let pokeName;
+let caughtPoke = []; 
+
 
 async function randomPokeGenerator() {
     const response = await fetch(url + randomPokeNum); 
     const poke = await response.json(); 
     console.log(poke);
 
+    // Get the name of the pokemon
     pokeName = poke.name;
+    // Show which pokemon appeared on the dashboard 
     const pokeAppearText = document.createElement('p'); 
     pokeAppearText.innerText = (pokeName + ' appeared!')
-
     pokeCard.appendChild(pokeAppearText);
 
+    createCatchBtn();
 }
 
-// 
+// Catch functionality 
+function createCatchBtn() {
+    const catchBtn = document.createElement('button'); 
+    catchBtn.textContent = ('Catch ' + pokeName); 
+
+    pokeCard.appendChild(catchBtn);
+
+}
 
 
 randomPokeGenerator();
