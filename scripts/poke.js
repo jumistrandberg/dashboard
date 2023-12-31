672dc4c1869aaa6c17b56904790696f41ace7019
Jumi <feliciastrandberg@gmail.com>
2023-12-31 03:05:15 +0100

    catch event is triggered by catch button works fine

--- scripts/poke.js
+++ scripts/poke.js
@@ -1,38 +1,61 @@
-// Get the elements 
-const pokeCard = document.getElementById('poke-card');
-const pokeBox = document.getElementById('poke-box'); 
+// Get the elements
+const pokeCard = document.getElementById("poke-card");
+const pokeBox = document.getElementById("poke-box");
+let pokeStatusText = document.getElementById("poke-status-text");
+let catchBtn;
 
 // Url to the national dex
-const url = 'https://pokeapi.co/api/v2/pokemon-species/';
+const url = "https://pokeapi.co/api/v2/pokemon-species/";
 const randomPokeNum = Math.floor(Math.random() * 1025 + 1);
-// The name of the random pokemon 
+// The name of the random pokemon
 let pokeName;
-let caughtPoke = []; 
-
+let caughtPoke = [];
 
 async function randomPokeGenerator() {
-    const response = await fetch(url + randomPokeNum); 
-    const poke = await response.json(); 
-    console.log(poke);
+  const response = await fetch(url + randomPokeNum);
+  const poke = await response.json();
+  console.log(poke);
 
-    // Get the name of the pokemon
-    pokeName = poke.name;
-    // Show which pokemon appeared on the dashboard 
-    const pokeAppearText = document.createElement('p'); 
-    pokeAppearText.innerText = (pokeName + ' appeared!')
-    pokeCard.appendChild(pokeAppearText);
+  // Get the name of the pokemon
+  pokeName = poke.name;
+  // Show which pokemon appeared on the dashboard
+  pokeStatusText.innerText = pokeName + " appeared!";
+  pokeCard.appendChild(pokeStatusText);
 
-    createCatchBtn();
+  createCatchBtn();
+  catchBtn.disabled = false;
+
 }
 
-// Catch functionality 
+// Create the catch button
 function createCatchBtn() {
-    const catchBtn = document.createElement('button'); 
-    catchBtn.textContent = ('Catch ' + pokeName); 
+  catchBtn = document.createElement("button");
+  catchBtn.textContent = "Catch " + pokeName;
 
-    pokeCard.appendChild(catchBtn);
+  pokeCard.appendChild(catchBtn);
 
+  // Trigger catch if button is
+  catchBtn.addEventListener("click", () => {
+    catchCondition(pokeName);
+  });
 }
 
+// Catch condition function
+function catchCondition(poke) {
+  const catchRate = poke.capture_rate;
+
+  // Generate the balls number between 0 and 255
+  const randomBallNum = Math.floor(Math.random() * 256);
+
+  // Check if capture is successful
+  if (catchRate <= randomBallNum) {
+    console.log("caught");
+  } else {
+    // The pokemon escapes on fail
+    pokeStatusText.innerText = pokeName + " got away!";
+    pokeCard.appendChild(pokeStatusText);
+    catchBtn.disabled = true;
+  }
+}
 
 randomPokeGenerator();
