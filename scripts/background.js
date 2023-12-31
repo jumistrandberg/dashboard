c2cccbdbc091d6b3b4f75d6739ffe586c5b52ff8
Jumi <feliciastrandberg@gmail.com>
2023-12-31 05:25:50 +0100

    fetching api key ok

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,18 @@
 // Get button 
 const bgBtn = document.getElementById('bg-btn'); 
 
+// Function to get the API key 
+async function getApiKey() {
+    const response = await fetch('./apiKeys.json')
+    if(response.ok) {
+        const keyArray = await response.json(); 
+        const bgApiKey = keyArray[1].unsplash;
+        return bgApiKey;
+
+    } else {
+        console.log(`Error: ${response.status}`);
+    }
+}
+
+
+getApiKey();
