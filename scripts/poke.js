88c5e27dbfedda722f556d0ed04ba17948169661
Jumi <feliciastrandberg@gmail.com>
2023-12-31 02:07:38 +0100

    getting random pokemon name works!

--- scripts/poke.js
+++ scripts/poke.js
@@ -1,12 +1,17 @@
 // Url to the national dex
 const url = 'https://pokeapi.co/api/v2/pokemon-species/';
 const randomPokeNum = Math.floor(Math.random() * 1025 + 1);
-
+// The name of the random pokemon 
+let pokeName;
 async function randomPokeGenerator() {
     const response = await fetch(url + randomPokeNum); 
     const poke = await response.json(); 
     console.log(poke);
 
+    pokeName = poke.name;
 }
+
+
+
 
 randomPokeGenerator();
