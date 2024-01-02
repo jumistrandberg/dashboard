780a5ecb238bc6baf5a3e83d6a885d532e8384f4
Jumi <feliciastrandberg@gmail.com>
2024-01-02 21:34:30 +0100

    fixed background random btn. forgot to add eventlistener to bgBtn

--- scripts/background.js
+++ scripts/background.js
@@ -1,53 +1,59 @@
-// Get elements
-const bgBtn = document.getElementById('bg-btn'); 
-const bgSearchInput = document.getElementById('bg-search-input'); 
-const bgSearchBtn = document.getElementById('bg-search-btn'); 
+document.addEventListener("DOMContentLoaded", () => {
+  // Get elements
+  const bgBtn = document.getElementById("bg-btn");
+  const bgSearchInput = document.getElementById("bg-search-input");
+  const bgSearchBtn = document.getElementById("bg-search-btn");
 
-
-// Function to get the API key 
-async function getApiKey() {
+  // Function to get the API key
+  async function getApiKey() {
     try {
-        const response = await fetch('./apiKeys.json');
-        if (response.ok) {
-            const keyArray = await response.json(); 
-            const bgApiKey = keyArray[1].unsplash;
-            return bgApiKey;
-        } else {
-            console.log(`Error: ${response.status}`);
-            return null;
-        }
-    } catch (error) {
-        console.error('Error fetching API key:', error);
+      const response = await fetch("./apiKeys.json");
+      if (response.ok) {
+        const keyArray = await response.json();
+        const bgApiKey = keyArray[1].unsplash;
+        return bgApiKey;
+      } else {
+        console.log(`Error: ${response.status}`);
         return null;
-    }
-}
-
-async function randomImg(search) { 
-    try {
-        const apiKey = await getApiKey(); 
-        const url = `https://api.unsplash.com/photos/random?query=${search}&client_id=${apiKey}`;
-        const response = await fetch(url);
-
-        if (response.ok) {
-            const data = await response.json();
-            const imgUrl = data.urls.regular;
-            // set as bg 
-            document.body.style.backgroundImage = `url('${imgUrl}')`; 
-            // document.body.style.backgroundSize = 'cover';
-            // document.body.style.backgroundPosition = 'center';
-            
-        } else {
-            console.log(`Error fetching random image: ${response.status}`);
-        }
+      }
     } catch (error) {
-        console.error('Error getting random image:', error);
+      console.error("Error fetching API key:", error);
+      return null;
     }
-}
+  }
 
-bgSearchBtn.addEventListener('click', (e) => {
-    const search = bgSearchInput.value; 
+  async function randomImg(search) {
+    try {
+      const apiKey = await getApiKey();
+      const url = `https://api.unsplash.com/photos/random?query=${search}&client_id=${apiKey}`;
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const data = await response.json();
+        const imgUrl = data.urls.regular;
+        // set as bg
+        document.body.style.backgroundImage = `url('${imgUrl}')`;
+        // document.body.style.backgroundSize = 'cover';
+        // document.body.style.backgroundPosition = 'center';
+      } else {
+        console.log(`Error fetching random image: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error getting random image:", error);
+    }
+  }
+
+  bgSearchBtn.addEventListener("click", (e) => {
+    const search = bgSearchInput.value;
     randomImg(search);
-    this.value = ''; 
-})
-// Call randomImg function with a search query (replace 'yourSearchQuery' with an actual search query)
-randomImg();
+    bgSearchInput.value = "";
+  });
+
+  bgBtn.addEventListener("click", () => {
+    randomImg();
+    console.log("random btn click");
+  });
+
+  // Call randomImg function with a search query (replace 'yourSearchQuery' with an actual search query)
+  randomImg();
+});
