469d9e6f7259a2502637f64316c8aa9f4d297024
Jumi <feliciastrandberg@gmail.com>
2024-01-11 14:27:05 +0100

    removed commentedout code of parts that work

--- scripts/poke.js
+++ scripts/poke.js
@@ -1,268 +1,276 @@
-document.addEventListener('DOMContentLoaded', () => {
-// Get the elements
-const pokeCard = document.getElementById('poke-card');
-const pokeBox = document.getElementById('poke-box');
-let pokeStatusText = document.getElementById('poke-status-text');
-let catchBtn;
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the elements
+  const pokeCard = document.getElementById("poke-card");
+  const pokeBox = document.getElementById("poke-box");
+  let pokeStatusText = document.getElementById("poke-status-text");
+  let catchBtn;
 
-// // Data Variables 
-// let poke;
-// // Store name of pokemon
-// let pokeName;
-// // Store type of pokemon
-// let pokeType;
-// // Get species data needed for catch rate
-// let species;
-// // Store catch rate of pokemon
-// let pokeCatchRate; 
-// // Store the sprite img 
-// let pokeImg; 
-let species;
-const speciesUrl = 'https://pokeapi.co/api/v2/pokemon-species/'
+  let species;
+  const speciesUrl = "https://pokeapi.co/api/v2/pokemon-species/";
 
+  // Url to the national dex
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+  let randomPokeNum = Math.floor(Math.random() * 1025 + 1);
 
-// Url to the national dex
-const url = 'https://pokeapi.co/api/v2/pokemon/';
-let randomPokeNum = Math.floor(Math.random() * 1025 + 1);
+  // Get the caught poke from local storage
+  let caughtPoke = JSON.parse(localStorage.getItem("caughtPoke")) || [];
+  // Show the party
+  displayCaughtPoke();
 
-// Class for pokemon
-class Pokemon {
-  constructor(name, type, catchRate, img) {
-    this.name = '';
-    this.type = '';
-    this.catchRate = '';
-    this.img = '';
-    this.speciesUrl = '';
-    this.species = ''
-  };
+  // Function to display the party and add remove icon
+  function displayCaughtPoke() {
+    pokeBox.innerHTML = "";
+    caughtPoke.forEach((poke) => {
+      const pokeDiv = document.createElement("div");
+      pokeDiv.textContent = poke;
 
-  // Method to get the species URL
-  getSpeciesUrl(url) {
-    this.speciesUrl = url;
+      const removeIcon = document.createElement("i");
+      removeIcon.classList.add("fas", "fa-times", "remove-icon");
+      removeIcon.setAttribute("title", "Remove Pokemon");
+      pokeDiv.appendChild(removeIcon);
+
+      pokeBox.appendChild(pokeDiv);
+
+      // Listen for clicks on remove icon and remove the Pokemon
+      removeIcon.addEventListener("click", () => {
+        const index = caughtPoke.indexOf(poke);
+        if (index !== -1) {
+          caughtPoke.splice(index, 1);
+          displayCaughtPoke();
+          localStorage.setItem("caughtPoke", JSON.stringify(caughtPoke));
+        }
+      });
+    });
   }
 
-  // Method to get the values 
-  async getPokeValues() {
-    // Fetch data from species URL
-    const response = await fetch(this.speciesUrl); 
-    const speciesData = await response.json(); 
+  // Class for pokemon
+  class Pokemon {
+    constructor(name, type, catchRate, img) {
+      this.name = "";
+      this.type = "";
+      this.catchRate = "";
+      this.img = "";
+      this.speciesUrl = "";
+      this.species = "";
+    }
 
-    // Set the values 
-    this.name = this.poke.species.name;
-    this.type = this.poke.types[0].type.name;
-    this.img = this.poke.sprites.other['official-artwork'].front_default;
-    this.catchRate = speciesData.capture_rate;
-  };
+    // Method to get the species URL
+    getSpeciesUrl(url) {
+      this.speciesUrl = url;
+    }
 
-  // Method to get the capture rate from species 
-  async getCatchRate() {
-    const response = fetch(speciesUrl + randomPokeNum); 
-    this.species = (await response).json();
-    species = this.species;
+    // Method to get the values
+    async getPokeValues() {
+      // Fetch data from species URL
+      const response = await fetch(this.speciesUrl);
+      const speciesData = await response.json();
 
-    return species;
-  };
+      // Set the values
+      this.name = this.poke.species.name;
+      this.type = this.poke.types[0].type.name;
+      this.img = this.poke.sprites.other["official-artwork"].front_default;
+      this.catchRate = speciesData.capture_rate;
+    }
 
-  // Method to fetch the main data 
-  async fetchPokemonData() {
-    const response = await fetch(url + randomPokeNum); 
-    this.poke = await response.json();
+    // Method to get the capture rate from species
+    async getCatchRate() {
+      const response = await fetch(speciesUrl + randomPokeNum);
+      this.species = (await response).json();
+      species = this.species;
+
+      return species;
+    }
+
+    // Method to fetch the main data
+    async fetchPokemonData() {
+      const response = await fetch(url + randomPokeNum);
+      this.poke = await response.json();
+    }
+
+   
   }
 
-}; 
+  // New Pokemon instance
+  const thisPoke = new Pokemon();
 
-// New Pokemon instance 
-const thisPoke = new Pokemon(); 
+  // Display Pokemon on UI function
+  async function displayPokeData() {
+    // Set the species URL of this pokemon
+    thisPoke.getSpeciesUrl(speciesUrl + randomPokeNum);
 
-// Display Pokemon on UI function
-async function displayPokeData() {
-  // Set the species URL of this pokemon 
-thisPoke.getSpeciesUrl(speciesUrl + randomPokeNum)
+    await thisPoke.fetchPokemonData();
+    await thisPoke.getCatchRate();
+    await thisPoke.getPokeValues();
+    console.log(thisPoke.catchRate);
 
-  await thisPoke.fetchPokemonData();
-  await thisPoke.getCatchRate();
-  await thisPoke.getPokeValues();
-  console.log(thisPoke.catchRate);
+    // Create img
+    const pokeImg = document.createElement("img");
+    pokeImg.classList.add("poke-img");
+    pokeImg.src = thisPoke.img;
 
-  // Create img 
-  const pokeImg = document.createElement('img'); 
-  pokeImg.classList.add('poke-img'); 
-  pokeImg.src = thisPoke.img; 
+    //  Show which pokemon appeared on the dashboard
+    pokeStatusText.innerText = thisPoke.name + " appeared!";
 
-  //  Show which pokemon appeared on the dashboard
-  pokeStatusText.innerText = thisPoke.name + ' appeared!';
+    pokeCard.appendChild(pokeStatusText);
+    pokeCard.appendChild(pokeImg);
 
+    createCatchBtn();
+  }
 
-  pokeCard.appendChild(pokeStatusText);
-  pokeCard.appendChild(pokeImg);
+  // Create the catch button
+  function createCatchBtn() {
+    catchBtn = document.createElement("button");
+    catchBtn.textContent = "Catch " + thisPoke.name;
 
-  createCatchBtn();
-};
+    pokeCard.appendChild(catchBtn);
 
-// Create the catch button
-function createCatchBtn() {
-  catchBtn = document.createElement('button');
-  catchBtn.textContent = 'Catch ' + thisPoke.name;
+    // Trigger catch if button is clicked
+    catchBtn.addEventListener("click", () => {
+      catchCondition();
+    });
+  }
 
-  pokeCard.appendChild(catchBtn);
+  // Check if catch is successful
+  function catchCondition() {
+    // Generate the balls number between 0 and 255
+    const randomBallNum = Math.floor(Math.random() * 256);
+    // Try randomBallNum against catchRate
+    if (thisPoke.catchRate <= randomBallNum) {
+      //Trigger catch success
+      console.log('success')
+    } else {
+      // Trigger catch fail
+      console.log("fail");
+   
+    }
+  }
 
-  // Trigger catch if button is clicked
-  catchBtn.addEventListener('click', () => {
-    catchCondition();
-  });
-}; 
-
+  //
 
   displayPokeData();
 
-// // Get the pokemon values
-// async function getPokeValues() {
-//   pokeName = poke.species.name;
-//   console.log(pokeName); 
-
-//   pokeType = poke.types[0].type.name; 
-//   console.log(pokeType);
-
-//   pokeImg = poke.sprites.dream_world.front_default;
-//   console.log(pokeImg);
-
-//   pokeCatchRate = species.capture_rate;
-//   console.log(pokeCatchRate); 
-// }
-
-// // Get the capture rate from species 
-// async function getCatchRate() {
-//   const speciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${randomPokeNum}/`;
-//    const response = await fetch(url + randomPokeNum); 
-//    species = await response.json(); 
  
-//    return species;
-// }
 
 
+  // Get the caught poke from local storage
+  // let caughtPoke = JSON.parse(localStorage.getItem('caughtPoke')) || [];
+  // // Show the party
+  // displayCaughtPoke();
 
+  // // The name of the random pokemon
+  // let pokeName;
 
-// Get the caught poke from local storage 
-// let caughtPoke = JSON.parse(localStorage.getItem('caughtPoke')) || [];
-// // Show the party 
-// displayCaughtPoke();
+  // async function randomPokeGenerator() {
+  //   const response = await fetch(url + randomPokeNum);
+  //   poke = await response.json();
+  //   console.log(poke);
 
-// // The name of the random pokemon
-// let pokeName;
+  //   // Get the name of the pokemon
+  //   pokeName = poke.name;
+  //   // Show which pokemon appeared on the dashboard
+  //   pokeStatusText.innerText = pokeName + ' appeared!';
+  //   pokeCard.appendChild(pokeStatusText);
 
-// async function randomPokeGenerator() {
-//   const response = await fetch(url + randomPokeNum);
-//   poke = await response.json();
-//   console.log(poke);
+  //   createCatchBtn();
+  //   catchBtn.disabled = false;
+  // }
 
-//   // Get the name of the pokemon
-//   pokeName = poke.name;
-//   // Show which pokemon appeared on the dashboard
-//   pokeStatusText.innerText = pokeName + ' appeared!';
-//   pokeCard.appendChild(pokeStatusText);
+  // // Create the catch button
+  // function createCatchBtn() {
+  //   catchBtn = document.createElement('button');
+  //   catchBtn.textContent = 'Catch ' + pokeName;
 
-//   createCatchBtn();
-//   catchBtn.disabled = false;
-// }
+  //   pokeCard.appendChild(catchBtn);
 
-// // Create the catch button
-// function createCatchBtn() {
-//   catchBtn = document.createElement('button');
-//   catchBtn.textContent = 'Catch ' + pokeName;
+  //   // Trigger catch if button is
+  //   catchBtn.addEventListener('click', () => {
+  //     catchCondition(pokeName);
+  //   });
+  // }
 
-//   pokeCard.appendChild(catchBtn);
+  // // Catch condition function
+  // function catchCondition() {
+  //   const catchRate = poke.capture_rate;
 
-//   // Trigger catch if button is
-//   catchBtn.addEventListener('click', () => {
-//     catchCondition(pokeName);
-//   });
-// }
+  //   // Generate the balls number between 0 and 255
+  //   const randomBallNum = Math.floor(Math.random() * 256);
 
-// // Catch condition function
-// function catchCondition() {
-//   const catchRate = poke.capture_rate;
+  //   // Check if capture is successful
+  //   if (catchRate <= randomBallNum) {
+  //     caughtPoke.push(pokeName);
+  //     displayCaughtPoke();
+  //     pokeStatusText.innerText = pokeName + ' was added to your party!';
+  //       // Save updated caughtPoke to localStorage
+  //       localStorage.setItem('caughtPoke', JSON.stringify(caughtPoke));
+  //       catchBtn.disabled = true;
 
-//   // Generate the balls number between 0 and 255
-//   const randomBallNum = Math.floor(Math.random() * 256);
+  //   } else {
+  //     // The pokemon escapes on fail
+  //     pokeStatusText.innerText = pokeName + ' got away!';
+  //     pokeCard.appendChild(pokeStatusText);
+  //     catchBtn.disabled = true;
 
-//   // Check if capture is successful
-//   if (catchRate <= randomBallNum) {
-//     caughtPoke.push(pokeName);
-//     displayCaughtPoke();
-//     pokeStatusText.innerText = pokeName + ' was added to your party!';
-//       // Save updated caughtPoke to localStorage
-//       localStorage.setItem('caughtPoke', JSON.stringify(caughtPoke));
-//       catchBtn.disabled = true;
+  //   }
+  //   newPoke()
+  //   catchBtn.remove();
+  // }
 
-//   } else {
-//     // The pokemon escapes on fail
-//     pokeStatusText.innerText = pokeName + ' got away!';
-//     pokeCard.appendChild(pokeStatusText);
-//     catchBtn.disabled = true;
+  // // Function to display the party and add remove icon
+  // function displayCaughtPoke() {
+  //     pokeBox.innerHTML = '';
+  //     caughtPoke.forEach((poke) => {
+  //       const pokeDiv = document.createElement('div');
+  //       pokeDiv.textContent = poke;
 
-//   }
-//   newPoke()
-//   catchBtn.remove();
-// }
+  //       const removeIcon = document.createElement('i');
+  //       removeIcon.classList.add('fas', 'fa-times', 'remove-icon');
+  //       removeIcon.setAttribute('title', 'Remove Pokemon');
+  //       pokeDiv.appendChild(removeIcon);
 
-// // Function to display the party and add remove icon
-// function displayCaughtPoke() {
-//     pokeBox.innerHTML = ''; 
-//     caughtPoke.forEach((poke) => {
-//       const pokeDiv = document.createElement('div');
-//       pokeDiv.textContent = poke;
-      
-//       const removeIcon = document.createElement('i');
-//       removeIcon.classList.add('fas', 'fa-times', 'remove-icon');
-//       removeIcon.setAttribute('title', 'Remove Pokemon');
-//       pokeDiv.appendChild(removeIcon);
-  
-//       pokeBox.appendChild(pokeDiv);
-  
-//       // Listen for clicks on remove icon and remove the Pokemon
-//       removeIcon.addEventListener('click', () => {
-//         const index = caughtPoke.indexOf(poke);
-//         if (index !== -1) {
-//           caughtPoke.splice(index, 1);
-//           displayCaughtPoke();
-//           localStorage.setItem('caughtPoke', JSON.stringify(caughtPoke));
-//         }
-//       });
-//     });
-//   }
+  //       pokeBox.appendChild(pokeDiv);
 
-//   function newPoke() {
-//     const newPokeBtn = document.createElement('button');
-//     newPokeBtn.textContent = 'Find more Pokemon';
+  //       // Listen for clicks on remove icon and remove the Pokemon
+  //       removeIcon.addEventListener('click', () => {
+  //         const index = caughtPoke.indexOf(poke);
+  //         if (index !== -1) {
+  //           caughtPoke.splice(index, 1);
+  //           displayCaughtPoke();
+  //           localStorage.setItem('caughtPoke', JSON.stringify(caughtPoke));
+  //         }
+  //       });
+  //     });
+  //   }
 
-//     pokeCard.append(newPokeBtn);
+  //   function newPoke() {
+  //     const newPokeBtn = document.createElement('button');
+  //     newPokeBtn.textContent = 'Find more Pokemon';
 
-//     newPokeBtn.addEventListener('click', () => {
-//       randomPokeNum = Math.floor(Math.random() * 1025 + 1); // Generate a new random Pokemon number
+  //     pokeCard.append(newPokeBtn);
 
-//       randomPokeGenerator();
-//       newPokeBtn.remove();
-//     })
-//   }
+  //     newPokeBtn.addEventListener('click', () => {
+  //       randomPokeNum = Math.floor(Math.random() * 1025 + 1); // Generate a new random Pokemon number
 
-//   // Come back to better functionality maybe
+  //       randomPokeGenerator();
+  //       newPokeBtn.remove();
+  //     })
+  //   }
 
-//   // function noPokeTimer() {
-//   //   // Disable catch button during the timer
-//   //   catchBtn.disabled = true;
-//   //   pokeStatusText.innerText = 'No Pokemon here...';
+  //   // Come back to better functionality maybe
 
-  
-//   //   // Set a random duration between 10 seconds to 5 minutes
-//   //   const randomDuration = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
-  
-//   //   // Wait for the random duration before enabling the catch button again
-//   //   setTimeout(() => {
-//   //     randomPokeNum = Math.floor(Math.random() * 1025 + 1); // Generate a new random Pokemon number
-//   //     catchBtn.disabled = false;
-//   //   }, randomDuration);
-//   // }
+  //   // function noPokeTimer() {
+  //   //   // Disable catch button during the timer
+  //   //   catchBtn.disabled = true;
+  //   //   pokeStatusText.innerText = 'No Pokemon here...';
 
-// randomPokeGenerator();
+  //   //   // Set a random duration between 10 seconds to 5 minutes
+  //   //   const randomDuration = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
 
+  //   //   // Wait for the random duration before enabling the catch button again
+  //   //   setTimeout(() => {
+  //   //     randomPokeNum = Math.floor(Math.random() * 1025 + 1); // Generate a new random Pokemon number
+  //   //     catchBtn.disabled = false;
+  //   //   }, randomDuration);
+  //   // }
+
+  // randomPokeGenerator();
 });
