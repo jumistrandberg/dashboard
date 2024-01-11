73ec9ebd5681467038265f92cfe18148e25271f5
Jumi <feliciastrandberg@gmail.com>
2024-01-11 10:51:21 +0100

    changed api url

--- scripts/poke.js
+++ scripts/poke.js
@@ -9,10 +9,10 @@ let poke;
 // Get the caught poke from local storage 
 let caughtPoke = JSON.parse(localStorage.getItem('caughtPoke')) || [];
 // Show the party 
-displayCaughtPoke()
+displayCaughtPoke();
 
 // Url to the national dex
-const url = 'https://pokeapi.co/api/v2/pokemon-species/';
+const url = 'https://pokeapi.co/api/v2/pokemon/';
 let randomPokeNum = Math.floor(Math.random() * 1025 + 1);
 // The name of the random pokemon
 let pokeName;
