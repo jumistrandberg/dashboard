d5160af21573ec7813e27cfff543cb448bf07c02
Jumi <feliciastrandberg@gmail.com>
2024-01-11 09:17:03 +0100

    moved icon display condition to displayData function. Made displayData async

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,97 +1,97 @@
-document.addEventListener('DOMContentLoaded', () => {
-    checkWeather('Malmo');
+document.addEventListener("DOMContentLoaded", () => {
+  checkWeather("Malmo");
 
+  // Get the elements
+  const searchBox = document.querySelector(".search input");
+  const searchBtn = document.querySelector(".search button");
+  const weatherIcon = document.querySelector(".weather-icon");
 
-// Get the elements 
-const searchBox = document.querySelector('.search input');
-const searchBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('.weather-icon');
+  // Get the base URL
+  const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-// Get the base URL 
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=';
+  // Store the response from the fetch
+  let response;
+  // Store the array key
+  let keyArray;
+  let weatherApiKey;
 
-// Store the response from the fetch 
-let response; 
-// Store the array key
-let keyArray; 
-let weatherApiKey; 
+  // Store the JSON response
+  let data;
 
-// Store the JSON response 
-let data;
-
-// Function to get the API key 
-async function getApiKey() {
-     response = await fetch('./apiKeys.json')
-    if(response.ok) {
-         keyArray = await response.json(); 
-         weatherApiKey = keyArray[0].weather;
-        return weatherApiKey;
+  // Function to get the API key
+  async function getApiKey() {
+    response = await fetch("./apiKeys.json");
+    if (response.ok) {
+      keyArray = await response.json();
+      weatherApiKey = keyArray[0].weather;
+      return weatherApiKey;
     } else {
-        console.log(`Error: ${response.status}`);
+      console.log(`Error: ${response.status}`);
     }
-}
+  }
 
-// Function to get the weather and display it on dashboard 
-async function checkWeather(city) {
-    //Get the key and URL 
-     apiKey = await getApiKey();     
-     response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);    
-     data = await response.json();
+  // Function to get the weather and display it on dashboard
+  async function checkWeather(city) {
+    //Get the key and URL
+    apiKey = await getApiKey();
+    response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+    data = await response.json();
 
     displayData();
-    // Check which weather and pick right img to show 
-    if(data.weather[0].main == 'Clouds'){
-        weatherIcon.src = 'imgs/clouds.png'
-    } else if (data.weather[0].main == 'Clear') {
-        weatherIcon.src = 'imgs/clear.png'
-    } else if (data.weather[0].main == 'Drizzle') {
-        weatherIcon.src = 'imgs/drizzle.png'
-    } else if (data.weather[0].main == 'Mist') {
-        weatherIcon.src = 'imgs/mist.png'
-    } else if (data.weather[0].main == 'Snow') {
-        weatherIcon.src = 'imgs/snow.png'
+  }
+
+  // Function to set the data on UI
+  async function displayData() {
+    // Change the HTML elements based on the data from the weather API
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    console.log(data.weather)
+
+    // Check which weather and pick right img to show
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "imgs/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "imgs/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "imgs/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "imgs/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "imgs/snow.png";
     }
-    
-}; 
+  }
 
-// Function to set the data on UI 
-function displayData() {
-   // Change the HTML elements based on the data from the weather API 
-   document.querySelector('.city').innerHTML = data.name;
-   document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
-   document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-   document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
-}
-
-
-// Listen for clicks on search for the city 
-searchBtn.addEventListener('click', () =>{
+  // Listen for clicks on search for the city
+  searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
-    searchBox.value = ''
+    searchBox.value = "";
+  });
+
+  getApiKey();
 });
 
-getApiKey();
-
-})
-
-// // Function to get users location to display local weather 
+// // Function to get users location to display local weather
 // function userLocation() {
 //     navigator.geolocation.getCurrentPosition(
 //         async function(position) {
-//             const long = position.coords.longitude; 
+//             const long = position.coords.longitude;
 //             const lat = position.coords.latitude;
 
-//             // Get the city based on coords 
-//             const apiKey = getApiKey(); 
+//             // Get the city based on coords
+//             const apiKey = getApiKey();
 //             const reverseGeoUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=1&appid=${apiKey}`;
-            
+
 //             try {
 //                 const response = await fetch(reverseGeoUrl);
 //                 if(response.ok) {
-//                     const data = await response.json; 
-//                     const city = data[0].name; 
+//                     const data = await response.json;
+//                     const city = data[0].name;
 
-//                     checkWeather(city); 
+//                     checkWeather(city);
 //                 } else {
 //                     console.log(`Error: ${response.status}`);
 //                 }
@@ -103,4 +103,3 @@ getApiKey();
 // }
 
 // userLocation();
-
